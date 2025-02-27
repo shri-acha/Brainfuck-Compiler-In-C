a34a92e323f1abe69900c6f42a315b28c1bbdb5d
shri-acha <happy.irhs@gmail.com>
2025-02-27 20:51:05 +0545

    added ast generation function

--- src/lexer.h
+++ src/lexer.h
@@ -6,6 +6,8 @@
 #include "vector.h"
 #include "stack.h"
 typedef enum TOKEN_TYPE {
+  PROGRAM_S,
+  PROGRAM_E,
   ADD_OPERATION,
   SUB_OPERATION,
   LSHIFT_OPERATION,
@@ -21,7 +23,27 @@ typedef struct TOKEN{
   char tok_val;
 }TOKEN;
 
+typedef struct AST_node{
+  struct AST* left;
+  struct AST* right;
+  TOKEN* token;
+}AST_node;
+
+typedef struct AST{
+  struct AST* root;
+}AST;
+
+AST_node* AST_node_create(TOKEN* token_val){
+  AST_node* buff = (AST_node* )malloc(sizeof(AST_node));
+  buff->left=NULL;
+  buff->right=NULL;
+  buff->token=token_val;
+  return buff;
+}
+ 
+
 TOKEN* tokenizer(char* program_stream,size_t size_of_stream){
+
   TOKEN* tok_arr =(TOKEN*) malloc(sizeof(TOKEN)*size_of_stream); 
   if (tok_arr == NULL) {exit(-1);}
   for (int i=0;i<size_of_stream-1;i++){
@@ -57,12 +79,17 @@ TOKEN* tokenizer(char* program_stream,size_t size_of_stream){
      }
     tok_arr[i].tok_val = program_stream[i];
   }
+
   return tok_arr;
+
 }
+
 int parser(TOKEN* token_arr,long token_size){
+
     STACK node_buf;
     initStack(&node_buf);
     for (int i=0;i<token_size-1;i++){
+
       if(token_arr[i].tok_t == SLOOP_OPERATION ){
         push(&node_buf,'[');
       }
@@ -74,9 +101,27 @@ int parser(TOKEN* token_arr,long token_size){
         }
       }
     }
+
     if (!isEmpty(&node_buf)){
       fprintf(stderr,"[ERROR] Missing ']'\n");
     } // Loop Integrity Check
+      //
+    AST ast_val;
+    TOKEN program_start;
+    program_start.tok_val='\x0';
+    program_start.tok_t=PROGRAM_S;
 
+    TOKEN program_end;
+    program_end.tok_val='\x0';
+    program_end.tok_t=PROGRAM_E;
+
+
+    STACK program_stack;
+    initStack(&program_stack);
+    
+    AST_node_create(&token_arr[0]); 
+    
+    
+    
 }
 #endif // !
